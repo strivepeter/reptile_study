10957bf39f5454b5eee7bcacc8b6f22b819b10bd
peter <2819149088@qq.com>
2019-12-11 12:09:14 +0800

    feat ：初始化爬虫的工程

---
+++ src/main/java/com/peter/reptile_study/ReptileStudyApplication.java
@@ -0,0 +1,13 @@
+package com.peter.reptile_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReptileStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReptileStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/peter/reptile_study/ReptileStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peter.reptile_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReptileStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
